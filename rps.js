6caeeb0ce057ada05e72545cfadb67088b848677
David Lazic <lazic.ddavid@gmail.com>
2025-07-21 14:57:48 +0200

    added playerScore

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
 document.getElementById("startBtn").addEventListener("click", function () {
   document.getElementById("gameOverlay").style.display = "none";
 });
+
+let playerScore = 0;
+let computerScore = 0;
