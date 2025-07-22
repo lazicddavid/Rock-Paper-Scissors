b87de6750510cab1e41a13ac64733814c8c202db
David Lazic <lazic.ddavid@gmail.com>
2025-07-22 22:31:07 +0200

    added computerScore

--- rps.js
+++ rps.js
@@ -4,3 +4,13 @@ document.getElementById("startBtn").addEventListener("click", function () {
 
 let playerScore = 0;
 let computerScore = 0;
+
+let playerChoice;
+let computerChoice;
+3;
+
+//kako da znam sta kompjuter igra?
+//napravi array choices ["rock","paper", "scissors"]
+//nakon sto igra igrac:
+
+let computerScore;
