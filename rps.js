bd563bef6332ce2ba7bcb17871f5188cc9c9843a
David Lazic <lazic.ddavid@gmail.com>
2025-07-24 20:06:09 +0200

    added computerChoice

--- rps.js
+++ rps.js
@@ -10,6 +10,10 @@ let computerChoice;
 
 const options = ["rock", "paper", "scessior"];
 
+ funciton computerChoice() {
+  
+ }
+
 //kako da znam sta kompjuter igra?
 //napravi array choices ["rock","paper", "scissors"]
 //nakon sto igra igrac:
