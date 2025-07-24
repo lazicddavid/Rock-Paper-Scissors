6b2decc230818cdc42aaa14631fcadac8f36a946
David Lazic <lazic.ddavid@gmail.com>
2025-07-24 20:05:18 +0200

    added options

--- rps.js
+++ rps.js
@@ -7,19 +7,13 @@ let computerScore = 0;
 
 let playerChoice;
 let computerChoice;
-3;
+
+const options = ["rock", "paper", "scessior"];
 
 //kako da znam sta kompjuter igra?
 //napravi array choices ["rock","paper", "scissors"]
 //nakon sto igra igrac:
 
-function proveriBroj(broj) {
-  if (broj > 10) {
-    console.log("Broj je veliki");
-  } else {
-    console.log("Broj je mali");
-  }
-}
-
-proveriBroj(122); // Ispisaće: Broj je veliki
-proveriBroj(5); // Ispisaće: Broj je mali
+//kako da znam sta kompjuter igra?
+//napravi array choices ["rock","paper", "scissors"]
+//nakon sto igra igrac:
