76340cd7d4c4f12b423f7cfaaacc32bccaf7ae38
David Lazic <lazic.ddavid@gmail.com>
2025-07-26 13:36:19 +0200

    added computerSign

--- rps.js
+++ rps.js
@@ -1,3 +1,14 @@
+////////// pravila za igru:
+
+//belezi se izbor playera
+//komp. generise nasumican izbor
+//uporedimo oba izbora
+//ako su isti, rez = neresen
+//ako igrac pobedjuje, dodaje ++ se jedan poen
+//ako komp. pobedjuje dodaje se ++ poen njemu
+//menja se rezultat i na ekranu(curent score)
+//ko prvi dodje do 5 poena, pobednik, igra se vraca na pocetak.
+
 document.getElementById("startBtn").addEventListener("click", function () {
   document.getElementById("gameOverlay").style.display = "none";
 });
@@ -14,35 +25,27 @@ function getComputerChoice() {
   return options[randomIndex];
 }
 
-computerSign = getComputerChoice();
-
 console.log(`Kompjuter je igrao ${computerSign}`);
 
 let playerChoice = "";
 
 document.getElementById("rock").addEventListener("click", function () {
   playerChoice = "rock";
-  console.log("Igrac je izabrao:" playerChoice);
+  console.log("Igrac je izabrao:", playerChoice);
+  computerSign = getComputerChoice();
+  console.log("Kompjuter je izabrao:", computerSign);
 });
-
 
 document.getElementById("paper").addEventListener("click", function () {
   playerChoice = "paper";
   console.log("Igrač je izabrao:", playerChoice);
+  computerSign = getComputerChoice();
+  console.log("Kompjuter je izabrao:", computerSign);
 });
 
 document.getElementById("scissors").addEventListener("click", function () {
   playerChoice = "scissors";
   console.log("Igrač je izabrao:", playerChoice);
+  computerSign = getComputerChoice();
+  console.log("Kompjuter je izabrao:", computerSign);
 });
-
-////////// pravila za igru:
-
-//belezi se izbor playera
-//komp. generise nasumican izbor
-//uporedimo oba izbora
-//ako su isti, rez = neresen
-//ako igrac pobedjuje, dodaje ++ se jedan poen
-//ako komp. pobedjuje dodaje se ++ poen njemu
-//menja se rezultat i na ekranu(curent score)
-//ko prvi dodje do 5 poena, pobednik, igra se vraca na pocetak.
