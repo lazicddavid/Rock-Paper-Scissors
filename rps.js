56acdcf05c19f67504ad4f6c7bc85e1e391111eb
David Lazic <lazic.ddavid@gmail.com>
2025-07-27 16:06:45 +0200

    added rock

--- rps.js
+++ rps.js
@@ -50,4 +50,4 @@ document.getElementById("scissors").addEventListener("click", function () {
 
 if (playerChoice === computerSign) {
   console.log("Nereseno!");
-} else if ((playerChoice === "rock" && computerSign === "scissors") || (playerChoice === "paper" &&  computerSign
+} else if ((playerChoice === "rock" && computerSign === "scissors") || (playerChoice === "paper" &&  computerSign === "rock"
